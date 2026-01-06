291eabe78fcc8189030da989d90c925492b10ca6
Kittienessless123 <paranina.thebell@gmail.com>
2026-01-06 07:36:23 +0700

    added base files all

--- services/reference-service/src/app.module.ts
+++ services/reference-service/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CountriesModule } from './countries/countries.module';
+import { CurrenciesModule } from './currencies/currencies.module';
+import { SkillsModule } from './skills/skills.module';
+import { CategoriesModule } from './categories/categories.module';
+import { LanguagesModule } from './languages/languages.module';
 
 @Module({
-  imports: [],
+  imports: [CountriesModule, CurrenciesModule, SkillsModule, CategoriesModule, LanguagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- services/user-service/src/app.module.ts
+++ services/user-service/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { ExperiencesModule } from './experiences/experiences.module';
+import { EducationsModule } from './educations/educations.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProfilesModule, ExperiencesModule, EducationsModule],
   controllers: [AppController],
   providers: [AppService],
 })

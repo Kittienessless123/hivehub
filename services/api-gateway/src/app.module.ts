549c2df0767f8fcd6eaa83079ca79c21a2486353
Kittienessless123 <paranina.thebell@gmail.com>
2026-01-06 07:35:48 +0700

    added base files

--- services/api-gateway/src/app.module.ts
+++ services/api-gateway/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProxyModule } from './proxy/proxy.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
+  imports: [ProxyModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
